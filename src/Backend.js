39ef5588fe41c347ee16eb5c7229839d6e191fb0
Oliver Solano <osolano01@gmail.com>
2017-02-10 23:05:43 -0800

    Get list of users online

--- src/Backend.js
+++ src/Backend.js
@@ -6,48 +6,64 @@ class Backend {
     pubnub = null;
     // initialize Firebase Backend
     constructor() {
+        console.log('Pubnub constructor');
+
         pubnub = new PubNub({
             subscribeKey: 'sub-c-a8e24fb2-ef3b-11e6-b753-0619f8945a4f',
             publishKey: 'pub-c-daf7877d-2242-4a2a-b3be-c22c1d5e1a3d',
             uuid: 'computer user'
         });
 
-        this.set
-
+        console.log('ID', pubnub.getUUID());
         pubnub.subscribe({
-            channels: ['global']
+            channels: ['global'],
+            withPrescense: true
         });
 
-        pubnubDemo.addListener({
+        pubnub.addListener({
             message: function(message){
-                console.log(message)
+                console.log('Listener Message', message);
             }
         });
 
         // View the UUID and state objects of those currently subscribed
-
-
     }
 
-
-    setUid(value) {
-        this.uid = value;
-    }
-    getUid() {
-        return this.uid;
+    sendMessage(message) {
+        console.log('Try sending a message');
+        pubnub.publish(
+            {
+                message: {
+                    text: message
+                },
+                channel: 'global',
+            },
+            function (status, response) {
+                if (status.error) {
+                    // handle error
+                    console.log(status)
+                } else {
+                    console.log("message Published w/ timetoken", response.timetoken)
+                }
+            }
+        );
     }
 
     listOfUsersOnline() {
+        console.log('LIST ID', pubnub.getUUID());
         pubnub.hereNow(
             {
                 channels: ['global'],
                 includeState: true
             },
             function(status, response){
-                console.log(response);
+                console.log('Pubnub', pubnub);
+                console.log('Status', status);
+                console.log('List Of Users Response', response);
             }
         );
     }
+    
 }
 /*
 // retrieve the messages from the Backend
--- src/components/OnlineUsersList.js
+++ src/components/OnlineUsersList.js
@@ -1,45 +1,64 @@
 import React from 'react';
+
 import {
-  StyleSheet,
-  Text,
-  View,
-  ListView
+    StyleSheet,
+    Text,
+    View,
+    ListView
 } from 'react-native';
+
+import Backend from '../Backend';
+
 
 import { Actions } from 'react-native-router-flux';
 
 export default class OnlineUsersList extends React.Component {
     state = {
-      name: '',
+        name: '',
     };
 
     constructor() {
-      super();
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.state = {
-        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-      };
+        super();
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+        };
     }
 
     render() {
-      return (
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
-      );
+        return (
+            <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+            />
+        );
+    }
+
+    componentDidMount() {
+        Backend.sendMessage('HELLO');
+        Backend.listOfUsersOnline();
+        //Backend.listOfUsersOnline();
+        /*
+        Backend.loadMessages((message) => {
+            this.setState((previousState) => {
+                return {
+                    messages: GiftedChat.append(previousState.messages, message),
+                };
+            });
+        });
+        */
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  label3: {
-    fontSize: 20,
-    color: 'white',
-    marginTop: 20,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    label3: {
+        fontSize: 20,
+        color: 'white',
+        marginTop: 20,
+    },
 
 });

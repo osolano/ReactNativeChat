56106ace523a72a1c782ba6a032e63e8b686fb0c
Oliver Solano <osolano01@gmail.com>
2017-02-12 11:53:18 -0800

    Added blank screen when no users signed on

--- src/components/OnlineUsersList.js
+++ src/components/OnlineUsersList.js
@@ -6,7 +6,9 @@ import {
     Text,
     View,
     ListView,
-    TouchableHighlight
+    TouchableHighlight,
+    Container,
+    Image
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux';
@@ -20,7 +22,7 @@ export default class OnlineUsersList extends React.Component {
             dataSource: ds.cloneWithRows(this.props.onlineUsers),
             isLoading: false,
             name: '',
-            onlineUsers: []
+            onlineUsers: this.props.onlineUsers,
         };
     }
 
@@ -28,22 +30,17 @@ export default class OnlineUsersList extends React.Component {
         Backend.listenToPresenceEvents((response) => {
             Backend.listOfUsersOnline((users) => {
                 let newUsers = users.occupants.filter(user => user.uuid !== Backend.uuid);
+                console.log('New Users', newUsers);
                 this.setState({
                   dataSource: this.state.dataSource.cloneWithRows(newUsers),
+                  onlineUsers: newUsers,
                 });
             });
-
         });
     }
 
     componentWillUnmount() {
         Backend.unsubscribe();
-    }
-
-    getTheData(callback) {
-        Backend.listOfUsersOnline((response) => {
-            callback(response.occupants);
-        });
     }
 
     renderRow(rowData, sectionID, rowID) {
@@ -57,22 +54,36 @@ export default class OnlineUsersList extends React.Component {
                 }}
             underlayColor='#dddddd'
             style={{height:44}}>
-            <View style={styles.row}>
-            <Text style={{fontSize: 20, color: '#000000', marginLeft: 12}} numberOfLines={1}>{rowData.uuid}</Text>
-            </View>
+                <View style={styles.row}>
+                    <Text style={{fontSize: 20, color: '#000000', marginLeft: 12}} numberOfLines={1}>{rowData.uuid}</Text>
+                </View>
             </TouchableHighlight>
         );
     }
 
+    listIsEmpty() {
+        return (this.state.userCount == 0);
+    }
+
     render() {
+        this.renderRow = this.renderRow.bind(this)
+        var blankView = <Text style={{marginTop: 50, fontSize: 24, textAlign: 'center'}}>There are no users online yet... </Text>
+        var blankImageView = <Image style={{ height: 300, resizeMode: 'contain', justifyContent: 'center'}} source={{uri: 'https://media.giphy.com/media/26BRuo6sLetdllPAQ/giphy.gif'}} />
+        var currentView =
+            <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+            enableEmptySections={true}
+            renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+            />
+        var blankViewRender = (this.state.dataSource.getRowCount() == 0) ? blankView : <View></View>
+        var blankImageViewRender = (this.state.dataSource.getRowCount() == 0) ? blankImageView : <View></View>
+
         return(
             <View>
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={this.renderRow}
-                enableEmptySections={true}
-                renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-                />
+                {currentView}
+                {blankViewRender}
+                {blankImageViewRender}
             </View>
         );
     }

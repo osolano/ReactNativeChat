78f2bdfbcfd239532b30682f3675416153f09df5
Oliver Solano <osolano01@gmail.com>
2017-02-16 15:32:05 -0800

    Added Authentication

---
+++ src/Authentication.js
@@ -0,0 +1,29 @@
+class Authentication {
+
+  hello() {
+    console.log('hello');
+  }
+
+  authenticateLogin(username, password) {
+    console.log('authenticateLogin', username, password);
+    return fetch('http://staging.pearl.dental/api/v1/users/sign_in', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      })
+    }).then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+}
+export default new Authentication();

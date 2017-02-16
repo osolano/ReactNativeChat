0587c7e0204516b30e34449b4e2d8eeb3a250821
Oliver Solano <osolano01@gmail.com>
2017-02-16 15:32:38 -0800

    Added username and password authentication validation.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Backend from '../Backend';
+import Authentication from '../Authentication';
 
 import {
   StyleSheet,
@@ -20,33 +21,68 @@ export default class Login extends React.Component {
     return (
         <Container style={{backgroundColor: 'steelblue'}}>
             <Content>
-                <TextInput placeholder='Username' style={styles.textInput} placeholderTextColor='mintcream'
-                onChangeText={(text) => { this.setState({ username: text }); }}
-                value={this.state.name}
+                <TextInput placeholder='Username' style={styles.usernameInput} placeholderTextColor='mintcream'
+                  onChangeText={(text) => { this.setState({ username: text }); }}
+                  value={this.state.name}
+                  autoCapitalize='none'
+                  autoCorrect={false}
                 />
-                <TextInput placeholder='Password' style={styles.textInput} placeholderTextColor='mintcream'
-                onChangeText={(text) => { this.setState({ password: text }); }}
-                value={this.state.name}
+                <TextInput placeholder='Password' style={styles.passwordInput} placeholderTextColor='mintcream'
+                  onChangeText={(text) => { this.setState({ password: text }); }}
+                  value={this.state.name}
+                  secureTextEntry={true}
+                  autoCorrect={false}
                 />
 
-                <Button style={{marginLeft: 20, marginRight: 20}} success block large onPress={() => {
-                    if (this.state.name.length > 0) {
-                        Backend.pubnubSetup(this.state.name);
-                        Backend.listOfUsersOnline((response) => {
-                            let onlineUsers = response.occupants.filter(user => user.uuid !== Backend.uuid);
-                            Actions.onlineUsersList({
-                              name: this.state.name,
-                              onlineUsers: onlineUsers
-                            });
-                        });
-                    }
-                }}>
+                <Button style={{marginLeft: 20, marginRight: 20}} success block large onPress={() => this.tapLoginButton() }>
                     <Text style={{color: 'white', fontSize: 18}}>Login</Text>
                 </Button>
             </Content>
         </Container>
     );
   }
+
+  tapLoginButton() {
+    if (this.inputIsValid()) {
+      Authentication.authenticateLogin(this.state.username,this.state.password).then((json) => {
+        this.pushOnlineUserListScreen(json.email);
+      });
+    }
+  }
+
+  inputIsValid(username, password) {
+    // just making an assumption on username length.
+    if (this.state.username.length > 2) {
+      console.log('Valid username');
+    } else {
+      console.log('Invalid username');
+      alert('Your username is a bit short.');
+      return false;
+    }
+
+    // just making an assumption on password length.
+    if (this.state.password.length > 6) {
+      console.log('Valid password');
+    } else {
+      console.log('Invalid password');
+      alert('Your password is a bit short');
+      return false;
+    }
+
+    return true;
+  }
+
+  pushOnlineUserListScreen(username) {
+    Backend.pubnubSetup(username);
+    Backend.listOfUsersOnline((response) => {
+        let onlineUsers = response.occupants.filter(user => user.uuid !== Backend.uuid);
+        Actions.onlineUsersList({
+          name: username,
+          onlineUsers: onlineUsers
+        });
+    });
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -55,8 +91,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'steelblue',
   },
-  textInput: {
+  usernameInput: {
     marginTop: 40,
+    color: 'white',
+    fontSize: 34,
+    textAlign: 'center',
+    height: 100,
+    flex: 1
+  },
+  passwordInput: {
+    marginTop: 10,
     color: 'white',
     fontSize: 34,
     textAlign: 'center',
